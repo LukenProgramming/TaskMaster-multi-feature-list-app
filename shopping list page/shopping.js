a62fb88761c81e31cb0d48646d1dc1794194feca
LukenProgramming <lukeiannapier@gmail.com>
2024-06-01 02:03:27 +0100

    more animations

--- shopping list page/shopping.js
+++ shopping list page/shopping.js
@@ -19,7 +19,7 @@ const getNewItem  = () => {
         const ul = document.getElementById ('list');
         const li = document.createElement('li');
         li.textContent = `${newItemQuantity} x ${newItemName}`;
-        ul.appendChild(li).classList('strikeable');
+        ul.appendChild(li).classList.add('strikeable','show');
         newItemNameField = "";
         newItemQuantityField = "";
     }
@@ -40,3 +40,18 @@ document.addEventListener('click', (event) => {
         event.target.classList.add('strikeable');
     }
 });
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        } 
+        else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
