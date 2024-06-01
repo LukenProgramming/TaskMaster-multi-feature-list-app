833a4077f8102735922587fc0499b61ebcae2da9
LukenProgramming <lukeiannapier@gmail.com>
2024-06-01 01:57:18 +0100

    created shopping list app, all it needs is some quantity validation changes

---
+++ shopping list page/shopping.js
@@ -0,0 +1,42 @@
+const submitBtn = document.getElementById('submitBtn');
+
+const getNewItem  = () => {
+    let newItemNameField = document.getElementById('itemName')
+    let newItemName = newItemNameField.value.trim();
+    let newItemQuantityField = document.getElementById('quantity');
+    let newItemQuantity = newItemQuantityField.value.trim();
+    console.log(newItemName);
+    console.log(newItemQuantity);
+
+    if (newItemName !== "" ){
+        while ((isNaN(newItemQuantity) && newItemQuantity !== "")||( newItemQuantity <= 0 && newItemQuantity !== "")) {
+            newItemQuantity = parseInt(prompt("Please enter a value greater than 0 for the quantity:"), 10);
+        }
+        if(newItemQuantity == ""){
+            newItemQuantity === 1;
+        }
+        
+        const ul = document.getElementById ('list');
+        const li = document.createElement('li');
+        li.textContent = `${newItemQuantity} x ${newItemName}`;
+        ul.appendChild(li).classList('strikeable');
+        newItemNameField = "";
+        newItemQuantityField = "";
+    }
+
+};
+
+submitBtn.onclick = () => {
+    getNewItem();
+};
+
+document.addEventListener('click', (event) => {
+    if (event.target.classList.contains('strikeable')) {
+        event.target.classList.remove('strikeable');
+        event.target.classList.add('strike-through');
+    }
+    else if (event.target.classList.contains('strike-through')) {
+        event.target.classList.remove('strike-through');
+        event.target.classList.add('strikeable');
+    }
+});
